6d20dc83e85e0875823669dbf39f1464007e1472
timmi129 <39489931+timmi129@users.noreply.github.com>
2022-01-19 12:20:55 +0300

    Update Figure.php

--- src/Figure.php
+++ src/Figure.php
@@ -1,14 +1,71 @@
 <?php
 
-class Figure {
-    protected $isBlack;
+use DTO\AvailableStepDTO;
 
-    public function __construct($isBlack) {
+class Figure {
+    protected int $steps = 0;
+    protected $isBlack;
+    protected $canJump = false;
+
+    public function __construct(bool $isBlack) {
         $this->isBlack = $isBlack;
+    }
+
+    public function getIsBlack(): bool
+    {
+        return $this->isBlack;
+    }
+
+    public function addStep(): void
+    {
+        $this->steps++;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCanJump(): bool
+    {
+        return $this->canJump;
     }
 
     /** @noinspection PhpToStringReturnInspection */
     public function __toString() {
         throw new \Exception("Not implemented");
+    }
+
+    public function isValidMove(
+        string $xFrom,
+        int $yFrom,
+        string $xTo,
+        int $yTo,
+        bool $isAttack
+    ): bool {
+
+        $availableSteps = $this->getSteps($xFrom, $yFrom, $isAttack);
+
+        foreach ($availableSteps as $validStep) {
+            if ($validStep->getX() === ord($xTo) && $validStep->getY() === $yTo && $validStep->isEatStep() === $isAttack) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return AvailableStepDTO[]
+     */
+    protected function getSteps(
+        string $xFrom,
+        int $yFrom,
+        bool $isAttack
+    ): array {
+        throw new \Exception("Not implemented");
+    }
+
+    protected function checkBorderTable(string $x, int $y): bool
+    {
+
     }
 }
